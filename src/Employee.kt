d7ac088d42ec8711694cb598818741dec41ecf5b
somiahgit <somiahjamal1997@gmail.com>
2020-09-13 18:28:22 +0300

    Initial commit

---
+++ src/Employee.kt
@@ -0,0 +1,5 @@
+class Employee(val firstName:String="",
+               val lastName:String="",
+               val id:String="",
+               val isManager: Boolean=true,
+               val managerID: String="")
